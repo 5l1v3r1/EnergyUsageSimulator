100b4cead3a319ff4bd784d095302aac73b98d63
Onur Destanoğlu <odestanoglu@gmail.com>
2015-07-03 00:19:25 +0300

    adding mainForm

---
+++ EnergyUsageSimulator/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EnergyUsageSimulator
+{
+    public partial class EnergySimulator : Form
+    {
+        public EnergySimulator()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void nodeManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NodeForm nf = new NodeForm();
+            nf.Show();
+        }
+    }
+}
